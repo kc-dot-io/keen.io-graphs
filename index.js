d12613260ae25fcd0ec53e60d051f05be54df44c
Kyle Campbell <mail@slajax.com>
2014-04-07 18:49:43 -0700

    added simple query API for metric and series

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-module.exports = function KeenGraphs(k) {
+
+var o = require('object');
+
+module.exports = function KeenGraphs(keen) {
 
   return {
-    PieChart: require('./graphs/pie')(k),
-    LineChart: require('./graphs/line')(k),
-    NumberChart: require('./graphs/number')(k),
-    Heatmap: require('./graphs/heatmap')(k)
+    graphs: {
+      pie: require('./lib/graphs/pie')(keen),
+      line: require('./lib/graphs/line')(keen),
+      number: require('./lib/graphs/number')(keen),
+      heatmap: require('./lib/graphs/heatmap')(keen)
+    },
+    getMetric: function(metric, options, next, done) {
+
+      if(!metric) throw new Error('keen.get requires a metric')
+
+      options = o.merge({
+        analysisType: "count",
+        timeframe: "this_7_days",
+        interval: "hourly"
+      }, options);
+
+      var d = new keen.Metric(metric, options);
+      d.getResponse(function(data) {
+        next(data, done || function() {});
+      });
+
+    },
+    getSeries: function(metric, options, next, done) {
+
+      if(!metric) throw new Error('keen.get requires a metric')
+
+      options = o.merge({
+        analysisType: "count",
+        timeframe: "this_7_days",
+        interval: "hourly"
+      }, options);
+
+      var d = new keen.Series(metric, options);
+      d.getResponse(function(data) {
+        next(data, done || function() {});
+      });
+
+    }
   };
 
 };
